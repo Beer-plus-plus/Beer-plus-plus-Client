0950ec5bebc2ff39c8b2c1299ffa6f8e3cee19b3
JuanVicenteVazquezG <juanvicentevazquezg@gmail.com>
2019-11-08 06:04:11 +0100

    deploy

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Logo = () => {
   return (
-    <div className="logo" style={{ width: '100%', margin: '50px auto 0 auto' }}>
-      <img src="Beer++logo.svg" alt="loading" style={{ width: '100%' }}></img>
+    <div className="logo" style={{ width: '100%', height: '300px', margin: '50px auto 0 auto' }}>
+      <img src="Beer++logo.svg" alt="loading" style={{ width: '100%', margin: '0' }}></img>
+      <h1 style={{fontSize: '50px', textAlign: 'center', margin: '0'}}>Beer ++</h1>
     </div>
   );
 };
