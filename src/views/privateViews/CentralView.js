826d006e53690d2838c4e1afae05a33b1c77f221
JuanVicenteVazquezG <juanvicentevazquezg@gmail.com>
2019-11-11 08:14:53 +0100

    deploy

--- src/views/privateViews/CentralView.js
+++ src/views/privateViews/CentralView.js
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { withAuth } from '../../Context/AuthContext';
-
+import './CentralView.css';
 
 class Central extends Component {
-
-
   render() {
- return (
+    return (
       <div>
-        <Link to="/beers">
-          <img src="beer.svg" alt="Beer menu" style={{ width: '50px' }}></img>
-        </Link>
-        <Link to="/user">
-          <img src="user.svg" alt="User Profile" style={{ width: '50px' }}></img>
-        </Link>
-        <button onClick={this.props.handleLogout} style={{ border: 'none', background:'transparent'}}>
-          <img src="logout.svg" alt="Beer menu" style={{ width: '50px' }}></img>
-        </button>
+        <div className="Left-up-menu">
+          <Link to="/beers">
+            <img src="beer.svg" alt="Beer menu" style={{ width: '50px' }}></img>
+          </Link>
+        </div>
+        <div className="right-up-menu">
+          <Link to="/user">
+            <img src="user.svg" alt="User Profile" style={{ width: '50px' }}></img>
+          </Link>
+        </div>
+        <div className="left-down-menu"></div>
+        <div className="right-down-menu">
+          <button onClick={this.props.handleLogout} style={{ border: 'none', background: 'transparent' }}>
+            <img src="logout.svg" alt="Beer menu" style={{ width: '50px' }}></img>
+          </button>
+        </div>
       </div>
     );
   }
